c3fa89b659f59348eb6b24b352844059af3d5703
Milan Šůstek <milan.sustek@gmail.com>
2017-10-25 00:49:20 +0200

    JUnit 4.12

--- git-mvn-seminar/car-shop/src/test/java/cz/fi/muni/carshop/CarShopStorageServiceTest.java
+++ git-mvn-seminar/car-shop/src/test/java/cz/fi/muni/carshop/CarShopStorageServiceTest.java
@@ -21,48 +21,48 @@ import cz.fi.muni.carshop.services.CarShopStorageServiceImpl;
 
 public class CarShopStorageServiceTest {
 
-	private CarShopStorageService service = new CarShopStorageServiceImpl();
+    private CarShopStorageService service = new CarShopStorageServiceImpl();
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
-	@Test()
-	public void testPriceCantBeNegative() {
-		// JUnit 4.11
-		thrown.expect(IllegalArgumentException.class);
-		// JUnit 4.12
-		// thrown.reportMissingExceptionWithMessage("We expect exception on
-		// negative price").expect(IllegalArgumentException.class);
+    @Test()
+    public void testPriceCantBeNegative() {
+        // JUnit 4.11
+        //thrown.expect(IllegalArgumentException.class);
+        // JUnit 4.12
+        thrown.reportMissingExceptionWithMessage("We expect exception on negative price")
+                .expect(IllegalArgumentException.class);
 
-		service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, -1));
-	}
+        service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, -1));
+    }
 
-	@Test
-	public void testGetCar() {
-		service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000));
+    @Test
+    public void testGetCar() {
+        service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000));
 
-		assertTrue(service.isCarAvailable(Color.BLACK, CarTypes.AUDI).isPresent());
-	}
+        assertTrue(service.isCarAvailable(Color.BLACK, CarTypes.AUDI).isPresent());
+    }
 
-	@Test
-	public void testCarShopStorage_containsTypeForExistingCar() {
-		service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000));
-		Map<CarTypes, List<Car>> cars = CarShopStorage.getInstancce().getCars();
+    @Test
+    public void testCarShopStorage_containsTypeForExistingCar() {
+        service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000));
+        Map<CarTypes, List<Car>> cars = CarShopStorage.getInstancce().getCars();
 
-		assertThat(cars, hasKey(CarTypes.AUDI));
-	}
+        assertThat(cars, hasKey(CarTypes.AUDI));
+    }
 
-	// expected to fail with JUnit < 4.11
-	@Test
-	public void testGetCheaperCars_returnsCorrectResult() {
-		service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000));
-		service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 889000));
-		service.addCarToStorage(new Car(Color.WHITE, CarTypes.AUDI, 2016, 859000));
-		service.addCarToStorage(new Car(Color.BLUE, CarTypes.AUDI, 2016, 909000));
+    // expected to fail with JUnit < 4.11
+    @Test
+    public void testGetCheaperCars_returnsCorrectResult() {
+        service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000));
+        service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 889000));
+        service.addCarToStorage(new Car(Color.WHITE, CarTypes.AUDI, 2016, 859000));
+        service.addCarToStorage(new Car(Color.BLUE, CarTypes.AUDI, 2016, 909000));
 
-		assertThat(service.getCheaperCarsOfSameTypeAndYear(new Car(Color.BLACK, CarTypes.AUDI, 2016, 900000)),
-				hasSize(3));
+        assertThat(service.getCheaperCarsOfSameTypeAndYear(new Car(Color.BLACK, CarTypes.AUDI, 2016, 900000)),
+                hasSize(3));
 
-	}
+    }
 
 }
